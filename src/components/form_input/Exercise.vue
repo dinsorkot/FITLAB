7f2d57507817aeffa4dfe550c48654e12e3e4cdb
Sunz098 <bazada147@gmail.com>
2023-03-03 14:35:45 +0700

    form

--- src/components/form_input/Exercise.vue
+++ src/components/form_input/Exercise.vue
@@ -1,108 +1,122 @@
 <script setup>
-import { ref } from 'vue';
-import { storeToRefs } from 'pinia'
-import { useExerciseStore } from '../../stores/dataExercise'
+import { ref } from "vue";
+import { storeToRefs } from "pinia";
+import { useExerciseStore } from "../../stores/dataExercise";
 import { db } from "/src/firebase.js";
 import { collection, addDoc } from "firebase/firestore";
 var selectedNumber = ref(0);
-const store = useExerciseStore()
+const store = useExerciseStore();
 const { data_hour } = storeToRefs(store);
-const exercisedata = ref({type:({}) , hour:0 , minute:0})
+const exercisedata = ref({ type: {}, hour: 0, minute: 0 });
 
-function showdata(){
-    console.log(exercisedata.value);
-}
-
-
-async function addexercisedata(){
-    try {
+async function addexercisedata() {
+  try {
     const docRef = await addDoc(collection(db, "Exercise"), exercisedata.value);
     console.log("Document written with ID: ", docRef.id);
-    alertify.alert('FITLAB', 'Success');
+    alertify.alert("FITLAB", "Success");
   } catch (e) {
     console.error("Error adding document: ", e);
   }
 }
 </script>
 <template>
-    <div class="container">
-        <div class="mb-3 d-flex align-items-center ">
-            <div class="sln">
-                จำนวนการออกกำลังกาย
-            </div>
-            <select class="form-select" aria-label="Default select example" v-model="selectedNumber">
-                <option v-for="n in 10" :value="n">{{ n }}</option>
-            </select>
-        </div>
-
-        <div class="row justify-content-center">
-            <div class=" col-md-4  mt-3" v-for="n in selectedNumber">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">ออกกำลังกาย</h5>
-                        <div>
-                            <div class="mb-3 in-form">
-                                <label for="exampleFormControlInput1" class="form-label">ประเภท</label>
-                                <select class="form-select" aria-label="Default select example" v-model="exercisedata.type">
-                                    <option v-for="key, value, n in data_hour" :key="n" :value="key">{{ key.name }}</option>
-                                </select>
-                            </div>
-                            <div class="mb-3 in-form">
-                                <label for="calorry" class="form-label">จำนวนแคลลอรี่</label>
-                                <div class="input-group mb-3">
-                                    <input type="number" class="form-control" placeholder="Recipient's username"
-                                        aria-label="Recipient's username" aria-describedby="basic-addon2" spellcheck="false"
-                                        data-ms-editor="true"
-                                        v-model="exercisedata.hour" >
-                                    <span class="input-group-text" id="basic-addon2">ชั่วโมง</span>
-                                </div>
-                                <div class="input-group mb-3">
-                                    <input type="number" class="form-control" placeholder="Recipient's username"
-                                        aria-label="Recipient's username" aria-describedby="basic-addon2" spellcheck="false"
-                                        data-ms-editor="true"
-                                        v-model="exercisedata.minute" >
-                                    <span class="input-group-text" id="basic-addon2">นาที</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="d-flex justify-content-center">
-                            <a class="btn btn-primary" @click="addexercisedata()">ยืนยัน</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
+  <div class="container">
+    <div class="mb-3 d-flex align-items-center">
+      <div class="sln">จำนวนการออกกำลังกาย</div>
+      <select
+        class="form-select"
+        aria-label="Default select example"
+        v-model="selectedNumber"
+      >
+        <option v-for="n in 10" :value="n">{{ n }}</option>
+      </select>
     </div>
+
+    <div class="row justify-content-center">
+      <div class="col-md-4 mt-3" v-for="n in selectedNumber">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">ออกกำลังกาย</h5>
+            <div>
+              <div class="mb-3 in-form">
+                <label for="exampleFormControlInput1" class="form-label">ประเภท</label>
+                <select
+                  class="form-select"
+                  aria-label="Default select example"
+                  v-model="exercisedata.type"
+                >
+                  <option v-for="(key, value, n) in data_hour" :key="n" :value="key">
+                    {{ key.name }}
+                  </option>
+                </select>
+              </div>
+              <div class="mb-3 in-form">
+                <label for="calorry" class="form-label">จำนวนแคลลอรี่</label>
+                <div class="input-group mb-3">
+                  <input
+                    type="number"
+                    class="form-control"
+                    placeholder="Recipient's username"
+                    aria-label="Recipient's username"
+                    aria-describedby="basic-addon2"
+                    spellcheck="false"
+                    data-ms-editor="true"
+                    v-model="exercisedata.hour"
+                  />
+                  <span class="input-group-text" id="basic-addon2">ชั่วโมง</span>
+                </div>
+                <div class="input-group mb-3">
+                  <input
+                    type="number"
+                    class="form-control"
+                    placeholder="Recipient's username"
+                    aria-label="Recipient's username"
+                    aria-describedby="basic-addon2"
+                    spellcheck="false"
+                    data-ms-editor="true"
+                    v-model="exercisedata.minute"
+                  />
+                  <span class="input-group-text" id="basic-addon2">นาที</span>
+                </div>
+              </div>
+            </div>
+            <div class="d-flex justify-content-center">
+              <a class="btn btn-primary" @click="addexercisedata()">ยืนยัน</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <style scoped>
 .card {
-    background-color: #1E1E1E;
+  background-color: #1e1e1e;
 }
 
 .sln {
-    width: 200px;
+  width: 200px;
 }
 
 .in-form input {
-    background-color: #222222;
-    border-color: #252525;
-    color: white;
-    height: 30px;
+  background-color: #222222;
+  border-color: #252525;
+  color: white;
+  height: 30px;
 }
 
 .in-form span {
-    background-color: #222222;
-    border-color: #252525;
-    color: white;
-    height: 30px;
+  background-color: #222222;
+  border-color: #252525;
+  color: white;
+  height: 30px;
 }
 
 .in-form input::placeholder {
-    color: transparent;
+  color: transparent;
 }
 
 .in-form label {
-    font-size: 15px;
+  font-size: 15px;
 }
 </style>
